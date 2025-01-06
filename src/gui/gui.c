b6fc212c02133d9f26fa952ae621705972f2a408
nongtajkrub <sunanborthwic@gmail.com>
2025-01-06 20:05:02 +0700

    Deleted input.c and input.h

--- src/gui/gui.c
+++ src/gui/gui.c
@@ -8,57 +8,6 @@ typedef struct {
 	ui_container_t container;
 } gui_t;
 
-void void_temp(void* arg) {
-	;;
-}
-
-bool up_trig_func(void* arg) {
-	return (kbio_ch == 'w');
-}
-
-bool down_trig_func(void* arg) {
-	return (kbio_ch == 's');
-}
-
-bool selc_trig_func(void* arg) {
-	return (kbio_ch == 'e');
-}
-
 void gui_main() {
-	ui_renderer_t renderer;
-	ui_renderer_make(&renderer, 60, 30);
 
-	ui_container_t container;
-	ui_container_init(&container);
-
-	ui_container_mk_and_set_header(&container, "Welcome!");
-	ui_container_mk_and_add_opt(&container, "Settings", void_temp, NULL);
-	ui_container_mk_and_add_opt(&container, "Credit", void_temp, NULL);
-	ui_container_mk_and_add_text(&container, "'q' to quit", COLOR_B | COLOR_GREEN | ALIGN_RIGHT);
-
-	ui_trig_t up_trig;
-	ui_trig_t down_trig;
-	ui_trig_t selc_trig;
-
-	ui_trig_make(&up_trig, up_trig_func, NULL);
-	ui_trig_make(&down_trig, down_trig_func, NULL);
-	ui_trig_make(&selc_trig, selc_trig_func, NULL);
-
-	ui_container_mk_and_set_selector(&container, up_trig, down_trig, selc_trig);
-
-	ui_renderer_ready();
-
-	while (TRUE) {
-		kbio_check_input();
-
-		if (kbio_ch == 'q') {
-			break;
-		}
-		if (ui_container_loop(&container)) {
-			ui_render_container(&renderer, &container);
-			ui_renderer_draw(&renderer);
-		}
-	}
-
-	ui_renderer_unready();
 }
