2e9cee3212e7ce13d115b16db5f9d84f54960d4d
nongtajkrub <sunanborthwic@gmail.com>
2025-02-06 18:28:06 +0700

    Added new packet req_resp_id_from_name (Untested).

---
+++ src/network/packet/req_resp_id_from_name.c
@@ -0,0 +1,19 @@
+#include "req_resp_id_from_name.h"
+
+bool pkt_make_req_id_from_name(pkt_req_id_from_name_t* pkt, char* name) {
+	const usize name_size = strlen(name);
+
+	if (name_size >= MAX_STUDENT_NAME_LEN - 1) {
+		return FALSE;
+	}
+
+	pkt_make_header(&pkt->header, REQ_ID, PKT_REQ_ID_FROM_NAME_PAYLOAD_SIZE);
+	memset(pkt->name, '\0', MAX_STUDENT_NAME_LEN);
+	memcpy(pkt->name, name, name_size);
+
+	return TRUE;
+}
+
+void pkt_make_resp_id_from_name(pkt_resp_id_from_name_t* pkt, i32 id) {
+	pkt->id = id;
+}
