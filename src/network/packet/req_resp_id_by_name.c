f9f53bee1b4f4dacd26e8fa0ce03c7e8bcb83f8c
nongtajkrub <sunanborthwic@gmail.com>
2025-02-06 21:16:00 +0700

    Change naming from 'from name' to 'by name'

--- src/network/packet/req_resp_id_by_name.c
+++ src/network/packet/req_resp_id_by_name.c
@@ -1,6 +1,6 @@
-#include "req_resp_id_from_name.h"
+#include "req_resp_id_by_name.h"
 
-bool pkt_make_req_id_from_name(pkt_req_id_from_name_t* pkt, char* name) {
+bool pkt_make_req_id_by_name(pkt_req_id_by_name_t* pkt, char* name) {
 	const usize name_size = strlen(name);
 
 	if (name_size >= MAX_STUDENT_NAME_LEN - 1) {
@@ -10,13 +10,13 @@ bool pkt_make_req_id_from_name(pkt_req_id_from_name_t* pkt, char* name) {
 	pkt_make_header(
 			&pkt->header,
 			REQ_ID_BY_NAME,
-			PKT_REQ_ID_FROM_NAME_PAYLOAD_SIZE);
+			PKT_REQ_ID_BY_NAME_PAYLOAD_SIZE);
 	memset(pkt->name, '\0', MAX_STUDENT_NAME_LEN);
 	memcpy(pkt->name, name, name_size);
 
 	return TRUE;
 }
 
-void pkt_make_resp_id_from_name(pkt_resp_id_from_name_t* pkt, i32 id) {
+void pkt_make_resp_id_by_name(pkt_resp_id_by_name_t* pkt, i32 id) {
 	pkt->id = id;
 }
---
+++ src/network/packet/req_resp_id_by_name.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#define GET_DATABASE_SETTINGS
+
+#include "../../settings.h"
+#include "packet.h"
+
+#define PKT_REQ_ID_BY_NAME_SIZE sizeof(pkt_req_id_by_name_t)
+#define PKT_REQ_ID_BY_NAME_PAYLOAD_SIZE \
+	PKT_REQ_ID_BY_NAME_SIZE - PKT_HEADER_SIZE
+typedef struct {
+	pkt_header_t header;
+
+	char name[MAX_STUDENT_NAME_LEN];
+} pkt_req_id_by_name_t;
+
+// return whether the packet is created
+bool pkt_make_req_id_by_name(pkt_req_id_by_name_t* pkt, char* name);
+
+#define PKT_RESP_ID_BY_NAME_SIZE sizeof(pkt_resp_id_by_name_t)
+#define PKT_RESP_ID_BY_NAME_PAYLOAD_SIZE \
+	PKT_RESP_ID_BY_NAME_SIZE - PKT_HEADER_SIZE
+typedef struct {
+	pkt_header_t header;
+
+	i32 id;
+} pkt_resp_id_by_name_t;
+
+void pkt_make_resp_id_by_name(pkt_resp_id_by_name_t* pkt, i32 id);
