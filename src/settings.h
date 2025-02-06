6309e54f367d9fb32cbaf0556684cd77aacfb44b
nongtajkrub <sunanborthwic@gmail.com>
2025-02-06 18:23:01 +0700

    Move MAX_STUDENT_NAME_LEN to settings.h

--- src/settings.h
+++ src/settings.h
@@ -4,3 +4,7 @@
 	#define ADDR "127.0.0.1"
 	#define PORT 8080 
 #endif // #ifdef GET_SERVER_INFO
+
+#ifdef GET_DATABASE_SETTINGS
+	#define MAX_STUDENT_NAME_LEN 128
+#endif // #ifdef GET_DATABASE_SETTINGS
