68d62183c03084b3f7a246a52fb09f07ec3943b1
nongtajkrub <sunanborthwic@gmail.com>
2025-02-11 19:59:25 +0700

    Implement network request data making system.

---
+++ src/network/request/data.c
@@ -0,0 +1,50 @@
+#include "data.h"
+
+void req_data_destroy(req_data_t* req_data) {
+	for (u16 i = 0; i < vec_size(req_data); i++) {
+		fix_string_t* data = VEC_GET(req_data, fix_string_t, i);
+		fix_string_destroy(data);
+	}
+
+	vec_destroy(req_data);
+}
+
+void req_data_add_str(req_data_t* req_data, const char* value) {
+	fix_string_t str;
+
+	fix_string_make(&str, strlen(value));
+	fix_string_set(&str, value);
+
+	vec_push(req_data, &str);
+}
+
+void req_data_add_int(req_data_t* req_data, int value) {
+	char i32_to_str_buf[MAX_I32_DIGIT + 1];
+	memset(i32_to_str_buf, '\0', MAX_I32_DIGIT + 1);
+	sprintf(i32_to_str_buf, "%d", value);
+
+	req_data_add_str(req_data, i32_to_str_buf);
+}
+
+void req_data_add_f32(req_data_t* req_data, f32 value) {
+	char f32_to_str_buf[MAX_F32_DIGIT + 1];
+	memset(f32_to_str_buf, '\0', MAX_F32_DIGIT + 1);
+	sprintf(f32_to_str_buf, "%f", value);
+
+	req_data_add_str(req_data, f32_to_str_buf);
+}
+
+void req_data_add_bool(req_data_t* req_data, bool value) {
+	if (value) {
+		req_data_add_str(req_data, "true");
+	} else {
+		req_data_add_str(req_data, "false");
+	}
+}
+
+void req_data_output(req_data_t* req_data) {
+	for (u16 i = 0; i < vec_size(req_data); i++) {
+		fix_string_t* data = VEC_GET(req_data, fix_string_t, i);
+		printf("%s\n", fix_string_get(data));
+	}
+}
---
+++ src/network/request/data.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <vector.h>
+#include <fix_string.h>
+
+typedef vec_t req_data_t;
+
+static inline void req_data_make(req_data_t* req_data) {
+	VEC_MAKE(req_data, fix_string_t);
+}
+
+void req_data_destroy(req_data_t* req_data);
+
+void req_data_add_str(req_data_t* req_data, const char* value);
+void req_data_add_i32(req_data_t* req_data, i32 value);
+void req_data_add_f32(req_data_t* req_data, f32 value);
+void req_data_add_bool(req_data_t* req_data, bool value);
+
+void req_data_output(req_data_t* req_data);
