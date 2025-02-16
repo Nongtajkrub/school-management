b19dde78890a1ba3c3cbe1040c5b289b59744fbb
nongtajkrub <sunanborthwic@gmail.com>
2025-02-16 22:08:38 +0700

    Improve var_string. Remove small_buf. Add new var_string_cat_char function.

--- include/var_string.c
+++ include/var_string.c
@@ -4,7 +4,7 @@
 #include <memory.h>
 #include <stdlib.h>
 
-static void alloc_str(var_string_t* str, usize size) {
+static void str_alloc(var_string_t* str, usize size) {
 	if (str->data != NULL) {
 		free(str->data);
 	}
@@ -16,7 +16,7 @@ static void alloc_str(var_string_t* str, usize size) {
 	memset(str->data, '\0', str->capacity + 1);
 }
 
-static void realloc_str(var_string_t* str, usize size) {
+static void str_realloc(var_string_t* str, usize size) {
 	str->capacity = size;
 	str->data = realloc(str->data, str->capacity + 1);
 	ASSERT(str->data != NULL, DEF_ALLOC_ERRMSG);
@@ -24,12 +24,7 @@ static void realloc_str(var_string_t* str, usize size) {
 	memset(str->data + str->len, '\0', (str->capacity - str->len) + 1);
 }
 
-static inline void clear_small_buf(var_string_t* str) {
-	memset(str->small_buf, '\0', VARSTR_SMALL_BUF_SIZE + 1);
-}
-
 void var_string_make(var_string_t* str) {
-	str->using_small_buf = FALSE;
 	str->capacity = 0;
 	str->len = 0;
 	// allocate only when needed
@@ -42,62 +37,54 @@ void var_string_make_and_set(var_string_t* str, const char* src) {
 }
 
 void var_string_destroy(var_string_t* str) {
-	str->using_small_buf = FALSE;
 	str->capacity = 0;
 	str->len = 0;
 
 	if (str->data != NULL) {
 		free(str->data);
 	}
-
-	clear_small_buf(str);
 }
 
 void var_string_set(var_string_t* str, const char* src) {
 	const usize src_len = strlen(src);
 
-	// if small enough use small buf instead
-	if (src_len < VARSTR_SMALL_BUF_SIZE) {
-		str->using_small_buf = TRUE;
-		clear_small_buf(str);
-		memcpy(str->small_buf, src, src_len);
-		return;
-	}
-
-	// if to big allocate more space first 
+	// if src to big allocate more space first 
 	if (src_len > str->capacity) {
-		str->using_small_buf = FALSE;
-		alloc_str(str, src_len);
+		str_alloc(str, src_len);
 	}
 
 	memcpy(str->data, src, src_len);
 	str->len = src_len;
 }
 
-const char* var_string_get(var_string_t* str) {
-	if (str->using_small_buf) {
-		return str->small_buf;
-	} else {
-		return str->data;
-	}
-}
-
-void var_string_cat(var_string_t* str, const char* src) {
-	const usize src_len = strlen(src);
+// ensure capacity is enought to cat string
+static void ensure_capacity_to_cat(var_string_t* str, usize src_len) {
 	const usize space_left = str->capacity - str->len;
 
-	// if not enough space and str is not new realloc the string else alloc
+	// if not enough space alloc more
 	if (space_left < src_len) {
 		str->capacity += src_len - space_left;
 
 		// if the string is not empty realloc it else alloc
 		if (str->data != NULL) {
-			realloc_str(str, str->capacity);
+			str_realloc(str, str->capacity);
 		} else {
-			alloc_str(str, str->capacity);
+			str_alloc(str, str->capacity);
 		}
 	}
+}
+
+void var_string_cat(var_string_t* str, const char* src) {
+	const usize src_len = strlen(src);
+	ensure_capacity_to_cat(str, src_len);
 
 	memcpy(str->data + str->len, src, src_len);
 	str->len += src_len;
+}
+
+void var_string_cat_char(var_string_t* str, char c) {
+	ensure_capacity_to_cat(str, 1);
+
+	str->data[str->len] = c;
+	str->len++;
 }
--- include/var_string.h
+++ include/var_string.h
@@ -2,17 +2,11 @@
 
 #include "type.h"
 
-#ifndef VARSTR_SMALL_BUF_SIZE
-	#define VARSTR_SMALL_BUF_SIZE 16
-#endif // #ifndef VARSTR_SMALL_BUF_SIZE
-
 typedef struct {
-	bool using_small_buf;
 	usize capacity;
 	usize len;
 
 	char* data;
-	char small_buf[VARSTR_SMALL_BUF_SIZE + 1];
 } var_string_t;
 
 void var_string_make(var_string_t* str);
@@ -20,9 +14,13 @@ void var_string_make_and_set(var_string_t* str, const char* src);
 void var_string_destroy(var_string_t* str);
 
 void var_string_set(var_string_t* str, const char* src);
-const char* var_string_get(var_string_t* str);
+
+static inline const char* var_string_get(var_string_t* str) {
+	return str->data;
+}
 
 void var_string_cat(var_string_t* str, const char* src);
+void var_string_cat_char(var_string_t* str, char c);
 
 static inline void var_string_output(var_string_t* str) {
 	printf("%s\n", var_string_get(str));
