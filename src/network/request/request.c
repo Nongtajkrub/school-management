0f15d4f776e012fe09bc6e60340f46683e324d32
nongtajkrub <sunanborthwic@gmail.com>
2025-02-18 19:30:44 +0700

    New request system using only string.

--- src/network/request/request.c
+++ src/network/request/request.c
@@ -1,68 +1,44 @@
 #include "request.h"
 #include "err_msg.h"
+#include "../networkio.h"
 
 #include <memory.h>
 #include <stdarg.h>
 
-void req_make(req_t* req, char* type) {
-	VEC_MAKE(req, fix_string_t);
-
-	// convert from c string to fix_string_t and push to req
-	fix_string_t type_fix_string = fix_string_convert_str(type);
-	vec_push(req, &type_fix_string);
+static inline void add_str(req_t* req, char* value) {
+	// + 1 for the extra '/' character
+	var_string_reserve(req, strlen(value) + 1);
+	var_string_cat(req, value);
+	var_string_cat_char(req, '/');
 }
 
-void req_destroy(req_t* req) {
-	for (u16 i = 0; i < vec_size(req); i++) {
-		fix_string_t* data = VEC_GET(req, fix_string_t, i);
-		fix_string_destroy(data);
-	}
-
-	vec_destroy(req);
-}
-
-void req_add_str(req_t* req, const char* value) {
-	fix_string_t str;
-
-	fix_string_make(&str, strlen(value));
-	fix_string_set(&str, value);
-
-	vec_push(req, &str);
-}
-
-
-void req_add_i32(req_t* req, i32 value) {
+static void add_i32(req_t* req, i32 value) {
 	char i32_to_str_buf[MAX_I32_DIGIT + 1];
 	memset(i32_to_str_buf, '\0', MAX_I32_DIGIT + 1);
 	sprintf(i32_to_str_buf, "%d", value);
 
-	req_add_str(req, i32_to_str_buf);
+	add_str(req, i32_to_str_buf);
 }
 
-void req_add_f32(req_t* req, f32 value) {
+static void add_f32(req_t* req, f32 value) {
 	char f32_to_str_buf[MAX_F32_DIGIT + 1];
 	memset(f32_to_str_buf, '\0', MAX_F32_DIGIT + 1);
 	sprintf(f32_to_str_buf, "%f", value);
 
-	req_add_str(req, f32_to_str_buf);
+	add_str(req, f32_to_str_buf);
 }
 
-void req_add_bool(req_t* req, bool value) {
+static void add_bool(req_t* req, bool value) {
 	if (value) {
-		req_add_str(req, "true");
+		add_str(req, "true");
 	} else {
-		req_add_str(req, "false");
+		add_str(req, "false");
 	}
 }
 
-void req_output(req_t* req) {
-	for (u16 i = 0; i < vec_size(req); i++) {
-		fix_string_t* data = VEC_GET(req, fix_string_t, i);
-		printf("%s\n", fix_string_get(data));
-	}
-}
+void req_make(req_t* req, char* type, char* fmt, ...) {
+	var_string_make(req);
 
-void req_add_fmt(req_t* req, char* fmt, ...) {
 	va_list data;
 	const char* c = fmt;
 
@@ -71,15 +47,18 @@ void req_add_fmt(req_t* req, char* fmt, ...) {
 	while (*c != '\0') {
 		switch (*c) {
 		case 'i':
-			req_add_i32(req, va_arg(data, i32));
+			add_i32(req, va_arg(data, i32));
 			break;
 		case 's':
-			req_add_str(req, va_arg(data, char*));
+			add_str(req, va_arg(data, char*));
 			break;
 		case 'f':
 			// have to use f64 to prevent promotion
-			req_add_f32(req, va_arg(data, f64));
+			add_f32(req, va_arg(data, f64));
 			break;
+		case 'b':
+			// have to use i32 to prevent promotion
+			add_bool(req, va_arg(data, i32));
 		default:
 			ASSERT(TRUE, REQ_DATA_ADD_INVALID_FMT_ERRMSG);
 			break;
--- src/network/request/request.h
+++ src/network/request/request.h
@@ -1,24 +1,25 @@
+// TODO: redo the request system
+
 #pragma once
 
 #include <vector.h>
-#include <fix_string.h>
+#include <var_string.h>
 #include <stdlib.h>
 
-typedef vec_t req_t;
+typedef var_string_t req_t;
 
 #define REQ_TYPE_ID_BY_NAME "ID_BY_NAME"
 
-void req_make(req_t* req, char* type);
-void req_destroy(req_t* req);
+// fmt - 'i' for int, 's' for string, 'f' for float, 'b' for bool
+void req_make(req_t* req, char* type, char* fmt, ...);
 
-void req_add_str(req_t* req, const char* value);
-void req_add_i32(req_t* req, i32 value);
-void req_add_f32(req_t* req, f32 value);
-void req_add_bool(req_t* req, bool value);
-// 'i' for int, 's' for string, 'f' for float
-void req_add_fmt(req_t* req, char* fmt, ...);
+static inline void req_destroy(req_t* req) {
+	var_string_destroy(req);
+}
 
-void req_output(req_t* req);
+static inline const char* req_get(req_t* req) {
+	return var_string_get(req);
+}
 
-bool req_send(req_t* req);
-bool req_recv(req_t* req);
+bool req_send(req_t* req, i32 sock_fd);
+bool req_recv(req_t* req, i32 sock_fd);
