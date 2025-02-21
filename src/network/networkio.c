927bb470ad9ba21ab7167a8aef6c3af84929e2aa
nongtajkrub <sunanborthwic@gmail.com>
2025-02-21 19:08:06 +0700

    Change coding style

--- src/network/networkio.c
+++ src/network/networkio.c
@@ -4,6 +4,7 @@
 #include <sys/socket.h>
 #include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 static bool send_block(i32 sockfd, const void* buf, usize size) {
 	if (send(sockfd, buf, size, 0) < 0) {
@@ -19,10 +20,7 @@ static bool send_none_block(i32 sockfd, const void* buf, usize size) {
 	while (total_sent != size) {
 		ssize sent = send(
 			sockfd,
-			(const char*)buf + total_sent,
-			size - total_sent,
-			MSG_DONTWAIT
-			);
+		   	(const char*)buf + total_sent, size - total_sent, MSG_DONTWAIT);
 
 		if (sent < 0) {
 			if (errno == EAGAIN || errno == EWOULDBLOCK) {
@@ -40,9 +38,8 @@ static bool send_none_block(i32 sockfd, const void* buf, usize size) {
 
 // return true if successfull else return false
 bool netio_send(i32 sockfd, const void* buf, usize size, bool block) {
-	return (block) ?
-		send_block(sockfd, buf, size) :
-		send_none_block(sockfd, buf, size);
+	return (block) ? 
+		send_block(sockfd, buf, size) : send_none_block(sockfd, buf, size);
 }
 
 static bool recv_block(i32 sockfd, void *buf, usize size) {
@@ -55,12 +52,11 @@ static bool recv_block(i32 sockfd, void *buf, usize size) {
 }
 
 static bool recv_none_block(i32 sockfd, void *buf, usize size) {
-	NETIO_LOG(DEF_NOT_IMPLEMENTED_ERRMSG);
+	ASSERT(TRUE, DEF_NOT_IMPLEMENTED_ERRMSG);
 	return FALSE;
 }
 
 bool netio_recv(i32 sockfd, void *buf, usize size, bool block) {
-	return (block) ?
-		recv_block(sockfd, buf, size) :
-		recv_none_block(sockfd, buf, size);
+	return (block) ? 
+		recv_block(sockfd, buf, size) : recv_none_block(sockfd, buf, size);
 }
