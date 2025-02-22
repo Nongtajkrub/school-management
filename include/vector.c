28ddd843943c7cf101c16de27bc286ebc92423c1
nongtajkrub <sunanborthwic@gmail.com>
2025-02-22 12:52:57 +0700

    New vector implementation

--- include/vector.c
+++ include/vector.c
@@ -1,64 +1,78 @@
 #include "vector.h"
 
-#include <stdlib.h>
 #include <memory.h>
 
-#define DEF_VEC_CAP 4
-
-static void alloc_all_elem(vec_t* vec) {
-	for (usize i = vec->size; i < vec->capacity; i++) {
-		vec->elem[i] = malloc(vec->elem_size);
-		ASSERT(vec->elem[i], DEF_ALLOC_ERRMSG);
-	}
-}
-
 void vec_make(vec_t* vec, usize elem_size) {
+	vec->capacity = VEC_STARTING_CAPACITY;
 	vec->size = 0;
-	vec->capacity = DEF_VEC_CAP;
 	vec->elem_size = elem_size;
-	vec->elem = calloc(DEF_VEC_CAP, sizeof(void*));
+
+	vec->elem = calloc(vec->capacity, sizeof(void*));
 	ASSERT(vec->elem != NULL, DEF_ALLOC_ERRMSG);
-	alloc_all_elem(vec);
 }
 
-// TODO: fix free invalid pointer
 void vec_destroy(vec_t* vec) {
-	printf("vec destroy!\n");
-
-	for (usize i = 0; i < vec_size(vec); i++) {
+	for (usize i = 0; i < vec->size; i++) {
 		free(vec->elem[i]);
 	}
 	free(vec->elem);
 
-	vec->capacity = DEF_VEC_CAP;
+	vec->capacity = 0;
 	vec->size = 0;
 	vec->elem_size = 0;
 }
 
-void vec_push(vec_t* vec, void* elem) {
-	if (vec->size >= vec->capacity) {
-		vec->capacity *= 2;
-		vec->elem = realloc(vec->elem, vec->capacity * sizeof(void*));
-		ASSERT(vec->elem != NULL, DEF_ALLOC_ERRMSG);
-		alloc_all_elem(vec);
+static void resize(vec_t* vec, usize size) {
+	if (size < VEC_STARTING_CAPACITY) {
+		size = VEC_STARTING_CAPACITY;
 	}
+
+	if (vec->capacity == size) {
+		return;
+	}
+
+	vec->capacity = size;
+	vec->elem = realloc(vec->elem, vec->capacity * sizeof(void*));
+	ASSERT(vec->elem != NULL, DEF_ALLOC_ERRMSG);
+}
+
+void vec_push(vec_t* vec, void* elem) {
+	if (vec->size == vec->capacity) {
+		resize(vec, vec->capacity * 2);
+	}
+
+	vec->elem[vec->size] = malloc(vec->elem_size);
+	ASSERT(vec->elem[vec->size] != NULL, DEF_ALLOC_ERRMSG);
 	memcpy(vec->elem[vec->size], elem, vec->elem_size);
 	vec->size++;
 }
 
-void vec_pop(vec_t* vec) {
+void* vec_pop(vec_t* vec) {
 	if (vec->size == 0) {
-		return;
+		return NULL;
 	}
-	free(vec_back(vec));
+
 	vec->size--;
+	void* pop_elem = vec->elem[vec->size];
+	vec->elem[vec->size] = NULL;
+
+	if (vec->size < vec->capacity / 4) {
+		resize(vec, vec->capacity / 2);
+	}
+
+	return pop_elem;
+}
+
+void* vec_get(vec_t* vec, u32 i) {
+	ASSERT(i < vec->size, DEF_OVERFLOW_ERRMSG);
+	return vec->elem[i];
 }
 
 void vec_clear(vec_t* vec) {
 	for (usize i = 0; i < vec->size; i++) {
 		free(vec->elem[i]);
 	}
-	free(vec->elem);
 
-	vec_make(vec, vec->elem_size);
+	vec->size = 0;
+	resize(vec, VEC_STARTING_CAPACITY);
 }
--- include/vector.h
+++ include/vector.h
@@ -2,36 +2,35 @@
 
 #include "type.h"
 
-typedef struct {
-	// metadata
-	usize capacity;
-	usize size;
-	usize elem_size;
-
-	// elements
-	void** elem;
-} vec_t;
+#ifndef VEC_STARTING_CAPACITY
+	#define VEC_STARTING_CAPACITY 4
+#endif // #ifndef VEC_STARTING_CAPACITY
 
 #define VEC_MAKE(VEC, TYPE) vec_make(VEC, sizeof(TYPE))
 #define VEC_PUSH(VEC, TYPE, ELEM)                                             \
-	do {                                                                        \
-		TYPE elem = ELEM;                                                         \
-		vec_push(VEC, (void*)&elem);                                              \
-	} while (0)
-#define VEC_POP(VEC) vec_pop(&VEC)
+	do {                                                                      \
+		TYPE elem = ELEM;                                                     \
+		vec_push(VEC, &elem);                                                 \
+	} while(0)
 #define VEC_GET(VEC, TYPE, I) (TYPE*)vec_get(VEC, I)
-#define VEC_BACK(VEC, TYPE) (TYPE*)vec_back(VEC) 
-#define VEC_SIZE(VEC) VEC.size
+
+typedef struct {
+	usize capacity;
+	usize size;
+
+	usize elem_size;
+	void** elem;
+} vec_t;
 
 void vec_make(vec_t* vec, usize elem_size);
 void vec_destroy(vec_t* vec);
-void vec_push(vec_t* vec, void* elem);
-void vec_pop(vec_t* vec);
-void vec_clear(vec_t* vec);
 
-static inline void* vec_get(vec_t* vec, usize i) {
-	return (i > vec->size) ? NULL : vec->elem[i];
-}
+void vec_push(vec_t* vec, void* elem);
+void* vec_pop(vec_t* vec);
+
+void* vec_get(vec_t* vec, u32 i);
+
+void vec_clear(vec_t* vec);
 
 static inline void* vec_back(vec_t* vec) {
 	return (vec->size == 0) ? NULL : vec->elem[vec->size - 1];
@@ -39,10 +38,6 @@ static inline void* vec_back(vec_t* vec) {
 
 static inline usize vec_size(vec_t* vec) {
 	return vec->size;
-}
-
-static inline usize vec_elem_size(vec_t* vec) {
-	return vec->elem_size;
 }
 
 static inline usize vec_mem_size(vec_t* vec) {
