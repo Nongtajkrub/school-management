f12b69431bef435afea9b52420145698b5eed97d
nongtajkrub <sunanborthwic@gmail.com>
2025-02-25 20:15:53 +0700

    New gui menu system

--- src/gui/gui_sys/menu.h
+++ src/gui/gui_sys/menu.h
@@ -1,23 +1,23 @@
 #pragma once
 
 #include "container.h"
-#include "selector.h"
 
-#include <type.h>
 #include <vector.h>
 
-typedef vec_t ui_container_group_t;
+typedef vec_t ui_menu_t;
 
-static inline void ui_container_group_init(ui_container_group_t* group) {
-	VEC_MAKE(group, ui_container_t);
+static inline void ui_menu_make(ui_menu_t* menu) {
+	VEC_MAKE(menu, ui_container_t);
 }
 
-// return menu id that was added
-u16 ui_container_group_add(ui_container_group_t* group, ui_container_t* con);
+static inline void ui_menu_set_current(ui_menu_t* menu, ui_container_t* con) {
+	vec_push(menu, con);
+}
 
-static inline ui_container_t* ui_container_group_get(
-	ui_container_group_t* group,
-	u16 id
-	){
-	return VEC_GET(group, ui_container_t, id);
+static inline ui_container_t* ui_menu_get_current(ui_menu_t* menu) {
+	return VEC_BACK(menu, ui_container_t); 
+}
+
+static inline void ui_menu_return(ui_menu_t* menu) {
+	vec_pop_back(menu);
 }
