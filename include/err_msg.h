e1207645f326491bbd33ed4674e7bf3c4f704627
nongtajkrub <sunanborthwic@gmail.com>
2025-02-28 22:19:04 +0700

    New vector error messages

--- include/err_msg.h
+++ include/err_msg.h
@@ -1,2 +1,7 @@
 #define FIX_STRING_INDEX_OUT_OF_BOUNDS_ERRMSG \
     "fix_string: cannot set character at an index beyond the string length"
+
+#define VEC_OUT_OF_BOUND_ERRMSG \
+	"vector: out of bound error"
+#define VEC_UNDERFLOW_ERRMSG \
+	"vector: underflow"
