5d1721d1155bc5918c4a948744d383f504170128
nongtajkrub <sunanborthwic@gmail.com>
2025-03-04 12:35:31 +0700

    Move client_t to global. Fix memeory leak.

--- src/network/client/client.c
+++ src/network/client/client.c
@@ -7,6 +7,7 @@
 #include "../message/request/request.h"
 #include "../message/request/req_type.h"
 #include "../networkio.h"
+#include "../../gui/gui.h"
 
 #include <type.h>
 #include <memory.h>
@@ -16,7 +17,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct {
+struct client {
 	// address infomation
 	struct sockaddr_in addr;
 
@@ -24,50 +25,47 @@ typedef struct {
 
 	// sockets
 	i32 sockfd;
-} client_t;
+} static cli;
 
-static void init(client_t* cli, u16 port, const char* addr) {
+typedef struct client client_t;
+
+static void init(u16 port, const char* addr) {
 	// create a client socket
-	cli->sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (cli->sockfd < 0) {
+	cli.sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (cli.sockfd < 0) {
 		perror(CREATE_SOCK_ERRMSG);
 		exit(EXIT_FAILURE);
 	}
 
-	cli->addr.sin_family = AF_INET;
-	cli->addr.sin_port = htons(port);
+	cli.addr.sin_family = AF_INET;
+	cli.addr.sin_port = htons(port);
 	
 	// convert address from text to binary
-	if (inet_pton(AF_INET, addr, &cli->addr.sin_addr) < 0) {
+	if (inet_pton(AF_INET, addr, &cli.addr.sin_addr) < 0) {
 		perror(INVALID_ADDR_ERRMSG);
 		exit(EXIT_FAILURE);
 	}
 }
 
-static void connect_to_serv(client_t* cli) {
-	if (connect(cli->sockfd,
-				(struct sockaddr*)&cli->addr, sizeof(cli->addr)) < 0) 
+static void connect_to_serv() {
+	if (connect(cli.sockfd,
+				(struct sockaddr*)&cli.addr, sizeof(cli.addr)) < 0) 
 	{
 		perror(CONNECT_ERRMSG);
 		exit(EXIT_FAILURE);
 	}
 }
 
-static void cli_init(client_t* cli) {
-	init(cli, PORT, ADDR);
-	connect_to_serv(cli);
+static void cli_init() {
+	init(PORT, ADDR);
+	connect_to_serv();
 }
 
-static void cli_deinit(client_t* cli) {
-	close(cli->sockfd);
+static void cli_deinit() {
+	close(cli.sockfd);
 }
 
-void cli_main() {
-	client_t cli;
-
-	cli_init(&cli);
-	cli.running = true;	
-
+bool cli_req_id_by_name(const char* name, u16* buf) {
 	msg_req_t req;
 
 	req_make(
@@ -75,18 +73,51 @@ void cli_main() {
 		REQT_ID_BY_NAME, REQT_ID_BY_NAME_DATA_FMT, "Taj Borthwick");
 
 	if (!msg_send(&req, cli.sockfd)) {
-		printf("did not send!\n");
-	} else {
-		printf("send request -> %s\n", var_string_get(&req));
+		return false;
 	}
+
+	msg_destroy(&req);
 
 	msg_req_t rep;
 
 	if (!msg_recv(&rep, cli.sockfd)) {
-		printf("Fail to recv\n");
-	} else {
-		printf("%s\n", msg_get_no_size(&rep));
+		msg_destroy(&rep);
+		return false;
 	}
 
-	cli_deinit(&cli);
+	char* data = msg_get_data(&rep);
+	//printf("data -> %s\n", data);
+	msg_get_data_destroy(data);
+
+	msg_destroy(&rep);
+	return true;
+}
+
+void cli_main() {
+	client_t cli;
+
+	cli_init();
+	cli.running = true;	
+
+	// memory leak test
+	for (u64 i = 0; i < 10000000000000; i++) {
+		if (i % 100000 == 0) {
+			printf("it -> %ld\n", i);
+		}
+
+		cli_req_id_by_name("Taj Borthwick", NULL);
+	}
+
+	printf("done\n");
+	cli_deinit();
+
+	/*
+	gui_init();
+
+	while (gui_should_run() || cli.running) {
+		gui_loop();
+	}
+
+	gui_deinit();
+	*/
 }
--- src/network/client/client.h
+++ src/network/client/client.h
@@ -1,3 +1,7 @@
 #pragma once
 
+#include <type.h>
+
 void cli_main();
+
+bool cli_req_id_by_name(const char* name, u16* buf);
